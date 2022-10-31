a86317ed8b91e21c4f21250eaef031e3de3f7c90
Sarah Pace <sarahpace37@gmail.com>
2022-10-31 13:49:13 -0700

    Initial commit

---
+++ Source/Patch_Dialog_CreateXenotype.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace XenotypeRandomizer
+{
+    [HarmonyPatch(typeof(Dialog_CreateXenotype))]
+    [HarmonyPatch("DoBottomButtons")]
+    public static class Patch_Dialog_CreateXenotype_DoBottomButtons
+    {
+        public static void Postfix(
+            Dialog_CreateXenotype __instance, 
+            Rect rect, 
+            ref List<GeneDef> ___selectedGenes, ref string ___xenotypeName, ref bool ___xenotypeNameLocked, ref XenotypeIconDef ___iconDef)
+        {
+            if (Widgets.ButtonText(new Rect(rect.x + rect.width/2 - 150f/2, rect.y, 150f, 38f), "XenotypeRandomizer_Randomize".Translate()))
+            {
+                SoundDefOf.Tick_High.PlayOneShotOnCamera(null);
+
+                XenotypeRandomizer.Randomize(___selectedGenes, ref ___iconDef);
+
+                if (!___xenotypeNameLocked)
+                {
+                    ___xenotypeName = GeneUtility.GenerateXenotypeNameFromGenes(___selectedGenes);
+                }
+
+                MethodInfo onGenesChanged = AccessTools.Method(typeof(Dialog_CreateXenotype), "OnGenesChanged");
+                onGenesChanged.Invoke(__instance, new object[] { });
+            }
+        }
+    }
+}
---
+++ Source/XenotypeRandomizerMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace XenotypeRandomizer
+{
+    public class XenotypeRandomizerMod : Mod
+    {
+        public const string PACKAGE_ID = "xenotyperandomizer.1trickPwnyta";
+        public const string PACKAGE_NAME = "Xenotype Randomizer";
+
+        public XenotypeRandomizerMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
